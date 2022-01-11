043308ef24f99bf82ffe4786ce11bb2d7a06a049
cleisommais <cleisommais@gmail.com>
2022-01-11 19:02:19 +0000

    Added the blockchain apis and postman collection

--- src/app.js
+++ src/app.js
@@ -1,22 +1,52 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const uuid = require('uuid');
+const nodeAddress = uuid.v1().split('-').join('');
+const Blockchain = require('./blockchain');
+const bitcoin = new Blockchain();
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // fetch entire blockchain
 app.get('/blockchain', (request, response) => {
-    response.send();
+    response.send(bitcoin);
 });
 
 //create a new transaction
-app.get('/transaction', (request, response) => {
-    response.send();
+app.post('/transaction', (request, response) => {
+    const lastBlock = bitcoin.createNewTransaction(
+        request.body.amount,
+        request.body.sender,
+        request.body.recipient
+    );
+    response.json({
+        message: `Transaction will be added in block ${lastBlock}`,
+    });
 });
 
 //mine a new block
 app.get('/mine', (request, response) => {
-    response.send();
+    const lastBlock = bitcoin.getLastBlock();
+    const previousBlockHash = lastBlock['hash'];
+    const currentBlockData = {
+        transactions: bitcoin.pendingTransactions,
+        index: lastBlock['index'] + 1,
+    };
+    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = bitcoin.hashBlock(
+        previousBlockHash,
+        currentBlockData,
+        nonce
+    );
+    //reward for who is mining
+    bitcoin.createNewTransaction(12.5, '00', nodeAddress);
+    const newBlock = bitcoin.createNewBlock(
+        nonce,
+        previousBlockHash,
+        blockHash
+    );
+    response.send(newBlock);
 });
 
 const PORT = process.env.PORT || 3000;
